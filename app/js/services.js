3df3d0493b6233a542cd82f912d02870e723c599
maki <maki19830926@gmail.com>
2013-12-16 09:33:29 +0900

    clean

--- app/js/services.js
+++
@@ -1,8 +0,0 @@
-var toriwasaServices = angular.module('toriwasaServices', ['ngResource']);
-
-toriwasaServices.factory('user', ['$http',
-  function($http){
-    return $http.get('api/user', {
-      query: {method:'GET'}
-    });
-  }]);
